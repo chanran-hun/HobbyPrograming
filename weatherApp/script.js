3e9db8cf68176d184815984e40aa1f5d3f3be173
Jeong Hun <jhunlee56@naver.com>
2024-11-01 22:53:23 +0900

    도시이름을 직접 입력이 아닌 선택으로

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -1,26 +1,32 @@
 const apiKey = 'ec466d3543563852e291e4637330cb81';
-const searchBtn = document.getElementById('searchBtn');
-const weatherInfo = document.getElementById('weatherInfo');
 
-searchBtn.addEventListener('click', () => {
-    const city = document.getElementById('city').value;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.cod === 200) {
-                const { main, weather } = data;
-                weatherInfo.innerHTML = `
-                    <h2>${data.name}</h2>
-                    <p>온도: ${main.temp} °C</p>
-                    <p>날씨: ${weather[0].description}</p>
-                    <img src="http://openweathermap.org/img/wn/${weather[0].icon}@2x.png" alt="Weather Icon">
+const now = new Date();
+function updateTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    document.getElementById("time").innerText = `현재 시간: ${hours}:${minutes}:${seconds}`;
+}
+setInterval(updateTime, 1000);  
+
+document.getElementById('get_weather').addEventListener('click', () => {
+    const city = document.getElementById('city_select').value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=kr`)
+        .then(response =>response.json())
+        .then(data =>  {
+            if(data.cod === 200){
+                const {main, weather}= data;
+                document.getElementById('weather_info').innerHTML = `
+                            <h2>${data.name}</h2>
+                            <p>온도: ${main.temp} °C</p>
+                            <p>날씨: ${weather[0].description}</p>
+                            <img src="http://openweathermap.org/img/wn/${weather[0].icon}@2x.png" alt="Weather Icon">
                 `;
             } else {
-                weatherInfo.innerHTML = `<p>도시를 찾을 수 없습니다.</p>`;
+                document.getElementById('weatherInfo').innerHTML = `<p>도시를 찾을 수 없습니다.</p>`;
             }
         })
-        .catch(err => {
-            weatherInfo.innerHTML = `<p>오류가 발생했습니다.</p>`;
-            console.error(err);
-        });
+        .catch(error => console.error('Error fetching weather data:', error));
 });
