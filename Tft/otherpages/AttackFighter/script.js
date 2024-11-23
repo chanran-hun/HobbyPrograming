38cddd31a16d77a16c58fa6c066b07a0b378f7ea
JEONG HUN LEE <jhunlee56@naver.com>
2024-11-24 00:20:28 +0900

    공격력 전사 수정 완료

--- Tft/otherpages/AttackFighter/script.js
+++ Tft/otherpages/AttackFighter/script.js
@@ -1,14 +1,14 @@
 const champions = [
-    { name: "카밀", cost: 5, imgUrl: "img/Camille.webp" },
-    { name: "브라이어", cost: 5, imgUrl: "img/Briar.webp" },
-    { name: "올라프", cost: 4, imgUrl: "img/Olaf.webp" },
-    { name: "뽀삐", cost: 1, imgUrl: "img/Poppi.webp" },
-    { name: "워윅", cost: 1, imgUrl: "img/Warwick.webp" },
-    { name: "헤카림", cost: 3, imgUrl: "img/Hecarim.webp" },
-    { name: "닐라", cost: 2, imgUrl: "img/Nilah.webp" },
-    { name: "피오라", cost: 4, imgUrl: "img/Fiora.webp" },
-    { name: "제이스", cost: 1, imgUrl: "img/Jayce.webp" },
-    { name: "아칼리", cost: 2, imgUrl: "img/Akali.webp" }
+    { name: "세비카", cost: 5, imgUrl: "img/Sevika.jpg" },
+    { name: "제이스", cost: 5, imgUrl: "img/Jayce.jpg" },
+    { name: "바이", cost: 4, imgUrl: "img/Vi.jpg" },
+    { name: "암베사", cost: 4, imgUrl: "img/Ambessa.jpg" },
+    { name: "갱플랭크", cost: 3, imgUrl: "img/Gangplank.jpg" },
+    { name: "카밀", cost: 2, imgUrl: "img/Camille.jpg" },
+    { name: "우르곳", cost: 2, imgUrl: "img/Urgot.jpg" },
+    { name: "녹턴", cost: 2, imgUrl: "img/Nocturne.jpg" },
+    { name: "다리우스", cost: 1, imgUrl: "img/Darius.jpg" },
+    { name: "바이올렛", cost: 1, imgUrl: "img/Violet.jpg" }
 ];
 
 const items = [
