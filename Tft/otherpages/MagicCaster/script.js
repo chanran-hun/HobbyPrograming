d353fbb59493c6f6a688a39c18f8ca9aeb731032
Jeong Hun <jhunlee56@naver.com>
2024-11-24 16:45:29 +0900

    주문력 마법사 수정중

--- Tft/otherpages/MagicCaster/script.js
+++ Tft/otherpages/MagicCaster/script.js
@@ -1,21 +1,5 @@
 const champions = [
-    { name: "노라와 유미", cost: 5, imgUrl: "img/Norra.webp" },
-    { name: "제라스", cost: 5, imgUrl: "img/Xerath.webp" },
-    { name: "밀리오", cost: 5, imgUrl: "img/Milio.webp" },
-    { name: "나미", cost: 4, imgUrl: "img/Nami.webp" },
-    { name: "라이즈", cost: 4, imgUrl: "img/Ryze.webp" },
-    { name: "베이가", cost: 3, imgUrl: "" },
-    { name: "직스", cost: 1, imgUrl: "" },
-    { name: "조이", cost: 1, imgUrl: "" },
-    { name: "아리", cost: 2, imgUrl: "" },
-    { name: "세라핀", cost: 1, imgUrl: "" },
-    { name: "소라카", cost: 1, imgUrl: "" },
-    { name: "바드", cost: 3, imgUrl: "" },
-    { name: "신드라", cost: 2, imgUrl: "" },
-    { name: "질리언", cost: 2, imgUrl: "" },
-    { name: "흐웨이", cost: 3, imgUrl: "" },
-    { name: "카시오페아", cost: 2, imgUrl: "" },
-    { name: "카르마", cost: 4, imgUrl: "" }
+    { name: "르블랑", cost: 5, imgUrl: "img/Leblanc.jpg" }
 ];
 
 const champContainer = document.getElementById("champ-container");
